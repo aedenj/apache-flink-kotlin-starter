2cfd5ddaef12528460cdcd58b0ee04cef5f70603
Aeden Jameson <aeden.jameson@gmail.com>
2022-03-02 23:24:33 -0800

    clean up classes

--- app/src/main/kotlin/apache/flink/kotlin/starter/JsonToAvro.kt
+++ app/src/main/kotlin/apache/flink/kotlin/starter/JsonToAvro.kt
@@ -4,14 +4,20 @@ import org.apache.avro.Schema
 import org.apache.avro.generic.GenericData
 import org.apache.avro.generic.GenericRecord
 import org.apache.flink.api.common.functions.RichMapFunction
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode
 
 class JsonToAvro(s: String) : RichMapFunction<ObjectNode, GenericRecord>() {
-    @Transient val schema = s
+    private val schemaString = s
+    @Transient private lateinit var schema: Schema
+
+    override fun open(parameters: Configuration?) {
+        super.open(parameters)
+        this.schema = Schema.Parser().parse(schemaString)
+   }
 
     override fun map(msg: ObjectNode): GenericRecord {
-        println("HELP ME: " + this.schema)
-        return GenericData.Record(Schema.Parser().parse(this.schema)).apply {
+        return GenericData.Record(this.schema).apply {
             put("name", msg.get("value").get("name").toString())
             put("device", msg.get("value").get("device").toString())
         }
